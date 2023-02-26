08ad56f5356823a3d8bc09636f5cf34fb2ede80d
Indah Tri Praesti <101540077+agnesindah3@users.noreply.github.com>
2023-02-26 10:18:10 +0700

    Add 2 more test

--- cypress/integration/login.ts
+++ cypress/integration/login.ts
@@ -1,22 +1,36 @@
 import {LoginPage} from "./pages/login_pages"
 import {DashboardPage} from "./pages/dashboard_pages"
 
-let loginPage = new LoginPage()
-let dashboardPage = new DashboardPage()
-const URL = 'https://www.saucedemo.com/'
+let loginPage = new LoginPage() // memanggil class LoginPage()
+let dashboardPage = new DashboardPage() //menanggil class DashboardPage()
+const URL = 'https://www.saucedemo.com/' // url
 
-it('Test LOGIN', () => {
-    loginPage.login(URL,'standard_user','secret_sauce')
+it('Test LOGIN', () => { // unit testing pada fungsi login dengan data yang valid
+    loginPage.login(URL,'standard_user','secret_sauce') // login page dengan parameter
     loginPage.assertLogin() 
 })
 
-it('Test Sauce Demo Invalid Password', () => {
+it('Test Sauce Demo Invalid Password', () => { // unite testing pada fungsi invalid pass
     loginPage.login(URL,'standard_user','invalidPass')
     loginPage.assertLoginFail()
 })
 
-it('Test Sauce Demo Sauce labs product backpack', () => {
+it('Test Sauce Demo Sauce labs product backpack', () => { // unit testing ketika login berhasil
     loginPage.login(URL,'standard_user','secret_sauce')
     loginPage.assertLogin()
     dashboardPage.sauceLabsBackpack() 
+})
+
+
+it('Test Sauce Demo Sauce labs product backpack add to cart', () => { 
+    loginPage.login(URL,'standard_user','secret_sauce')
+    loginPage.assertLogin()
+    dashboardPage.addToCart() 
+})
+
+
+it('Test Sauce Demo Sauce labs product backpack remove from cart', () => { 
+    loginPage.login(URL,'standard_user','secret_sauce')
+    loginPage.assertLogin()
+    dashboardPage.removeFromCart() 
 })
