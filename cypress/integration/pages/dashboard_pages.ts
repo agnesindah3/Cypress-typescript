0b14be9a2d1ea51bd60c504a1a0c8193bd30c1b6
Indah Tri Praesti <101540077+agnesindah3@users.noreply.github.com>
2023-02-26 10:17:04 +0700

    Update dashboard_pages.ts

--- cypress/integration/pages/dashboard_pages.ts
+++ cypress/integration/pages/dashboard_pages.ts
@@ -1,8 +1,19 @@
 export class DashboardPage{
-    link_sauceLabsBackpack = 'Sauce Labs Backpack'
+    link_sauceLabsBackpack = 'Sauce Labs Backpack'    // variabel yang mengandung nilai 'Sauce Labs backpack
 
     sauceLabsBackpack(){
+        cy.contains(this.link_sauceLabsBackpack).click() // klik atribute html yang mengandung kata dari variabel link_sauceLabsBackpack
+        cy.contains('Sauce Labs Backpack').should('be.visible') // atribute yang mengandung 'Sauce Labs Backpack' seharusnya tampil
+    }
+
+    addToCart(){
         cy.contains(this.link_sauceLabsBackpack).click()
-        cy.contains('Sauce Labs Backpack').should('be.visible') 
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click() // add to cart
+    }
+
+    removeFromCart(){
+        cy.contains(this.link_sauceLabsBackpack).click()
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click() // remove from cart
     }
 }
